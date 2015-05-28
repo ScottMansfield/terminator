367090f5c3a78d6eb2650c1405d7f56ee3d19149
Scott Mansfield <sgmansf@gmail.com>
2015-05-27 23:54:23 -0700

    Adding tests as driver classes.

---
+++ src/test/java/com/widowcrawler/terminator/parse/ParserTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2015 Scott Mansfield
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.widowcrawler.terminator.parse;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * @author Scott Mansfield
+ */
+public class ParserTest {
+
+    @Test(timeout = 10000)
+    public void parse_validRobotsTxtSmall_parsesFile() throws Exception {
+        InputStream inputStream = new FileInputStream("src/test/resources/example_robots_small.txt");
+
+        Parser parser = new Parser(inputStream);
+
+        long start = System.nanoTime();
+        parser.parse();
+        long duration = System.nanoTime() - start;
+
+        System.out.println("Duration: " + duration);
+    }
+
+    @Test(timeout = 10000)
+    public void parse_validRobotsTxtLarge_parsesFile() throws Exception {
+        InputStream inputStream = new FileInputStream("src/test/resources/example_robots.txt");
+
+        Parser parser = new Parser(inputStream);
+
+        long start = System.nanoTime();
+        parser.parse();
+        long duration = System.nanoTime() - start;
+
+        System.out.println("Duration: " + duration);
+    }
+}
